115e37553d4dfbd16bb17c0f0cd4fc17d15ded76
UdsonWillams <udson.willams@gmail.com>
2023-06-16 01:19:38 -0300

    Commit dia 1

---
+++ Dia 1/day_one.py
@@ -0,0 +1,23 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+# Basicamente uma Copia do projeto original
+# COnteudo inicial muito basico.
+
+print(2 + 3)   # addition(+)
+print(3 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex # numero complexo, python tbm consegue entender-los nativamente.
+print(type('Udsoul'))           # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Uds'})) # Dictionary - dicionario, desordenado e aceita todos os valores.
+print(type({9.8, 3.14, 2.7}))    # Set - set é desordenado mas não pode ter valores repetidos.
+print(type((9.8, 3.14, 2.7)))    # Tuple - tupla é ordenada mas imutavel
